a0aa4b7370f08617e5e0c20fdbe69c67eef9d340
Muskan <me1222025@iitd.ac.in>
2024-07-18 21:52:06 +0530

    Add zkDApp directory

--- zkDApp/hardhat.config.ts
+++ zkDApp/hardhat.config.ts
@@ -3,10 +3,21 @@ import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-circom";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  // solidity: "0.8.17",
+  solidity: "0.6.11",
+  paths: {
+    artifacts: "./artifacts",
+  },
   circom: {
     inputBasePath: "./circuits",
-    ptau: "https://hermezptau.blob.core.windows.net/ptau/powersOfTau28_hez_final_15.ptau",
+
+    // ptau: "https://hermezptau.blob.core.windows.net/ptau/powersOfTau28_hez_final_15.ptau",
+    // ptau:"https://zk-snarks.s3.amazonaws.com/powersOfTau28_hez_final_15.ptau",
+    // ptau:"./ptau/powersOfTau28_hez_final_15.ptau",
+    // we can download the powersOfTau28_hez_final_15.ptau and give path to the hardhat-circom
+    ptau:"/home/muskan/zkDapp/Applied-ZKP-Workshop/zkDApp/ptau/powersOfTau28_hez_final_15.ptau",
+
+
     circuits: [
       { name: "group"}
     ]
